1def1432a907dd0dedc84e7a61c182f8bace870f
rellis14 <rellis14@myseneca.ca>
2022-06-11 14:52:05 -0400

    lab5 update

--- Lab3.py
+++ Lab3.py
@@ -5,7 +5,11 @@ def printName(n, f):
   file.write(n)
   file.close()
   
+def helloWorld():
+  print('Hello World')
+  
 if __name__ == '__main__':
   name = input("Please enter your name: ")
   filename = input("Please enter a filename: ")
   printName(name, filename)
+  helloWorld()
