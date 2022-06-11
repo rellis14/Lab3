7db3bfe8489263c57f79f467f299d0adba9375d8
rellis14 <106385875+rellis14@users.noreply.github.com>
2022-06-11 14:39:13 -0400

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import sys
-def printVars:
-  var1 = sys.argv[1]
-  var2 = sys.argv[2]
-  print('Variable1 = ' + var1)
-  print('Variable2 = ' + var2)
-  print('The script and variables used are', str(sys.argv))
+
+def printName(n, f):
+  file = open (f, 'w')
+  file.write(n)
+  file.close()
   
 if __name__ == '__main__':
-  printVars()
+  name = input("Please enter your name: ")
+  filename = input("Please enter a filename: ")
+  printName(name, filename)
